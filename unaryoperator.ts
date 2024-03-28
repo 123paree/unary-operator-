a7e1d25da3be057ae42d7304762cd40c14546861
Pareeshay Syed <137789313+123paree@users.noreply.github.com>
2024-03-28 22:30:42 +0500

    unaryoperator.ts

--- unaryoperator.ts
+++ unaryoperator.ts
@@ -1,5 +1,5 @@
 let a:number=5;
 let b:number=2;
-//  c = 6   +  7  +  1  +  0  + 7 +  1  + 1
+//  c = 6   +  7  +  1  +  0  + 7 +  1  + 1 = 23
 let c:number= ++a + ++a + --b + b-- + a + b++ + b;
 console.log(c);
